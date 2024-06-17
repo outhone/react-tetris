ddf1f4fefb63a0e78eba84c7233d810c406cc408
Outhone Bounkhoun <outhoneb@gmail.com>
2024-06-17 08:41:46 -0700

    Updated placeholder highscore and fixed lint and table warnings

--- src/components/home/Highscore.tsx
+++ src/components/home/Highscore.tsx
@@ -1,26 +1,29 @@
 const Highscore = () => {
   // Fetch highscore from database
   const highScoreData = [
-    { name: 'John', score: 1000 },
-    { name: 'James', score: 2000 },
-    { name: 'Jake', score: 3000 },
-    { name: 'Jim', score: 4000 },
+    { name: 'Coming Soon', score: 10000 },
+    { name: 'Coming Soon', score: 9000 },
+    { name: 'Coming Soon', score: 8000 },
+    { name: 'Coming Soon', score: 7000 },
+    { name: 'Coming Soon', score: 6000 },
   ];
 
   return (
     <>
       <h1 className="text-white">HighScore</h1>
       <table>
-        <tr className="tbl-header">
-          <th>Player</th>
-          <th>Score</th>
-        </tr>
-        {highScoreData.map((playerScore) => (
-          <tr key={`${playerScore.name}-${playerScore.score}`}>
-            <td>{playerScore.name}</td>
-            <td>{playerScore.score}</td>
+        <tbody>
+          <tr className="tbl-header">
+            <th>Player</th>
+            <th>Score</th>
           </tr>
-        ))}
+          {highScoreData.map((playerScore) => (
+            <tr key={`${playerScore.name}-${playerScore.score}`}>
+              <td>{playerScore.name}</td>
+              <td>{playerScore.score}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
