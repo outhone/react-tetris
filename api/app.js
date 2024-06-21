0f63a6813deea2340072f4b243ce45f44ce0d08f
Outhone Bounkhoun <outhoneb@gmail.com>
2024-06-21 09:56:39 -0700

    Fixed env used for cors and added prisma generate to script

--- api/app.js
+++ api/app.js
@@ -9,7 +9,6 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-app.use(cors({ origin: [process.env.REACT_APP_API_URL] }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({ origin: [process.env.REACT_APP_URL] }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
