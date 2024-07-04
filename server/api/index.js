816023886d3bb1211be4f3aa713be88d368b28be
Outhone Bounkhoun <outhoneb@gmail.com>
2024-07-03 20:03:06 -0700

    Allow multiple origins for CORS

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: [process.env.REACT_APP_URL] }));
+app.use(cors({ origin: process.env.REACT_APP_URL.split(',') }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
